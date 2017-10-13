ff36c1bc82671d770af2a6c2956117f2b5e6c974
Akshansh Jain <akshanshjn9@gmail.com>
2017-10-14 00:31:47 +0530

    Pre Release V1.0. Splash Animations added.

--- app/src/main/java/com/madhouseapp/kidslearningapp/SplashActivity.java
+++ app/src/main/java/com/madhouseapp/kidslearningapp/SplashActivity.java
@@ -1,13 +1,16 @@
 package com.madhouseapp.kidslearningapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,6 +18,9 @@ import java.util.TimerTask;
 public class SplashActivity extends AppCompatActivity {
 
     private ImageView madhouseLogo;
+    private TextView kidsLearning, from;
+    private ImageView image1, image2, image3;
+    private ConstraintLayout parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +31,56 @@ public class SplashActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        Timer timer = new Timer();
-
         madhouseLogo = (ImageView) findViewById(R.id.splash_logo);
-        Animation translate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate);
-        translate.reset();
-        madhouseLogo.clearAnimation();
-        madhouseLogo.startAnimation(translate);
+        kidsLearning = (TextView) findViewById(R.id.text_kids_learning);
+        from = (TextView) findViewById(R.id.text_from_splash);
+        image1 = (ImageView) findViewById(R.id.splash_image1);
+        image2 = (ImageView) findViewById(R.id.splash_image2);
+        image3 = (ImageView) findViewById(R.id.splash_image3);
+        parent = (ConstraintLayout) findViewById(R.id.parent_splash);
 
+        Typeface edo = Typeface.createFromAsset(getAssets(), "fonts/edosz.ttf");
+        kidsLearning.setTypeface(edo);
+        from.setTypeface(edo);
+
+        Animation zoom0 = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
+        Animation zoom1 = AnimationUtils.loadAnimation(this, R.anim.zoom1);
+        Animation zoom2 = AnimationUtils.loadAnimation(this, R.anim.zoom2);
+        Animation zoom3 = AnimationUtils.loadAnimation(this, R.anim.zoom3);
+        Animation zoom4 = AnimationUtils.loadAnimation(this, R.anim.zoom4);
+        Animation zoom5 = AnimationUtils.loadAnimation(this, R.anim.zoom5);
+
+        zoom0.reset();
+        kidsLearning.clearAnimation();
+        kidsLearning.startAnimation(zoom0);
+
+        zoom1.reset();
+        image1.clearAnimation();
+        image1.startAnimation(zoom1);
+
+        zoom2.reset();
+        image2.clearAnimation();
+        image2.startAnimation(zoom2);
+
+        zoom3.reset();
+        image3.clearAnimation();
+        image3.startAnimation(zoom3);
+
+        zoom4.reset();
+        from.clearAnimation();
+        from.startAnimation(zoom4);
+
+        zoom5.reset();
+        madhouseLogo.clearAnimation();
+        madhouseLogo.startAnimation(zoom5);
+
+        Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 startActivity(new Intent(getApplicationContext(), LandingActivity.class));
                 finish();
             }
-        }, 1500);
+        }, 3000);
     }
 }
